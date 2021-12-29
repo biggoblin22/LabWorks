057225465f7d29e601c0c3ea3d22e0d3a8e11295
Dima <dima.vinok@mail.ru>
2021-12-29 17:27:28 +0700

    Перевод "Лаб 01" в "Lab 01" Добавлена Лабораторная №2

---
+++ Lab's/Lab 01/Лаб 01.cpp
@@ -0,0 +1,31 @@
+﻿/*
+********************************
+*Винокуров Дмитрий ПИз-211     *
+*29.12.2021                    *
+*Лабораторная работа №1        *
+********************************
+*/
+
+
+
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double Length = 0.994;
+	int Angle = 50;
+	double SpeedUpPowerWeight = 9.80665;
+	double Period1, Period2, Period3;
+
+	Period1 = 2 * M_PI * pow((Length / SpeedUpPowerWeight), 0.5);
+	Period2 = 2 * M_PI * sqrt((Length / SpeedUpPowerWeight) * (1 + (0.0625 * pow(Angle, 2))));
+	Period3 = 2 * M_PI * sqrt((Length / SpeedUpPowerWeight) * (1 + ((0.25 * pow(sin(Angle / 2), 2)) + (0.140625 * pow(sin(Angle / 2), 4)))));
+
+	cout << Period1 << endl;
+	cout << Period2 << endl;
+	cout << Period3 << endl;
+}
---
+++ Lab's/Lab 02/Lab 02.cpp
@@ -0,0 +1,26 @@
+﻿/*
+********************************
+*Винокуров Дмитрий ПИз-211     *
+*29.12.2021                    *
+*Лабораторная работа №2        *
+********************************
+*/
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    double mass0 = 1125;
+    double time0 = 20.5;
+    int daysMassive[6] = { 1, 2, 5, 10, 20, 50 };
+    double DeltaMass;
+    for (int i = 0; i < 6; i++)
+    {
+        DeltaMass = mass0 * exp(-(log(2) / time0) * daysMassive[i]) * (exp(log(2) / time0) - 1);
+        cout.precision(4);
+        cout << DeltaMass << endl;
+    }
+    
+}
