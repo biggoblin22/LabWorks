10405e7eb8a93a5c896aff64c7264afde9acf051
biggoblin22 <74752991+biggoblin22@users.noreply.github.com>
2022-06-07 16:15:52 +0700

    Добавлена лабораторная работа 01 на с#

---
+++ Lab's/Lab01/Lab01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab01
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a;
+            Double b;
+            Double c;
+
+            Console.Write("Введите число ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите степень в которую хотите возвесли число " + a + " ");
+            b = Convert.ToDouble(Console.ReadLine());
+            c = a;
+            while (b>1) 
+            {  
+                a *= c;
+                b -= 1;
+            };
+
+            Console.WriteLine("Результат: " + a);
+        }
+    }
+}
